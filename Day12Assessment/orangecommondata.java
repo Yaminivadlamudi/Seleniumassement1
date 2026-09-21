572b0ccaba8bdb2d1eef5b7ec4985f8521374c9a
Yaminivadlamudi <161056580+Yaminivadlamudi@users.noreply.github.com>
2026-09-21 18:21:08 +0530

    Add OrangeCommonData class for test data management

---
+++ Day12Assessment/orangecommondata.java
@@ -0,0 +1,89 @@
+package Day12;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.DataProvider;
+
+   public class OrangeCommonData {
+	String browser;
+	public String url;
+	public String username;
+	public String password;
+	
+   public void data() throws IOException {
+		
+		FileInputStream fis = new FileInputStream("./src/test/resources/Day12/Orange.properties");
+		Properties p = new Properties();
+		p.load(fis);
+		
+		browser = p.getProperty("browser");
+		url = p.getProperty("url");
+		username = p.getProperty("username");
+		password = p.getProperty("password");
+		
+		
+    }
+  @DataProvider
+  public Object[][] vacancyData()throws EncryptedDocumentException, IOException {
+
+     FileInputStream fis =new FileInputStream("./src/test/resources/Day12/OrangeHRM.xlsx");
+     Workbook wb = WorkbookFactory.create(fis);
+     Sheet sh = wb.getSheet("Sheet1");
+     int rowCount = sh.getLastRowNum();
+     Object[][] obj = new Object[rowCount][5];
+     for (int row = 1; row <= rowCount; row++) {
+        obj[row - 1][0]=sh.getRow(row).getCell(0).getStringCellValue();
+        obj[row - 1][1]=sh.getRow(row).getCell(1).getStringCellValue();
+        obj[row - 1][2]=sh.getRow(row).getCell(2).getStringCellValue();
+        obj[row - 1][3]=sh.getRow(row).getCell(3).getStringCellValue();
+        obj[row - 1][4]=sh.getRow(row).getCell(4).getStringCellValue();
+    }
+
+    wb.close();
+    fis.close();
+
+    return obj;
+}
+  @DataProvider
+  public Object[][] employeeData()
+          throws EncryptedDocumentException, IOException {
+
+      FileInputStream fis =
+              new FileInputStream("./src/test/resources/Day12/OrangeHRM.xlsx");
+
+      Workbook wb = WorkbookFactory.create(fis);
+
+      Sheet sh = wb.getSheet("Sheet1");
+
+      int rowCount = sh.getLastRowNum();
+
+      Object[][] obj = new Object[rowCount][3];
+
+      for (int row = 1; row <= rowCount; row++) {
+
+          obj[row - 1][0] =
+                  sh.getRow(row).getCell(5).getStringCellValue();
+
+          obj[row - 1][1] =
+                  sh.getRow(row).getCell(6).getStringCellValue();
+
+          obj[row - 1][2] =
+                  sh.getRow(row).getCell(7).getStringCellValue();
+      }
+
+      wb.close();
+      fis.close();
+
+      return obj;
+  }
+
+
+  
+
+}
